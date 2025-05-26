d73916913cf3e6083d1e1c880959c31985002193
04mmii <04mmii@naver.com>
2025-05-26 17:00:35 +0900

    반응형 수정중

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy:
       command === "serve"
         ? {
             "/api": {
-              target: "http://localhost:3000", // vercel dev 실행 주소
+              target: "http://localhost:3000",
               changeOrigin: true,
               rewrite: (path) => path,
             },
