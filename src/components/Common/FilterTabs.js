b22c2cc57e1ea53ef1c3cd9f34df7fab0eecb14a
04mmii <04mmii@naver.com>
2025-05-30 16:54:31 +0900

    페이지 수정

--- src/components/Common/FilterTabs.js
+++ src/components/Common/FilterTabs.js
@@ -6,6 +6,6 @@ const FilterTabs = ({ selected, onSelect, options, }) => {
         }
         return value === selected;
     };
-    return (_jsx("div", { className: "flex flex-wrap justify-center gap-2 my-4", children: options.map(({ label, value }) => (_jsx("button", { className: `px-4 py-1 rounded-full border ${isSelected(value) ? "bg-black text-white" : "bg-white text-black"}`, onClick: () => onSelect(value), children: label }, label))) }));
+    return (_jsx("div", { className: "flex flex-wrap gap-2 my-4", children: options.map(({ label, value }) => (_jsx("button", { className: `px-4 py-1 rounded-full border ${isSelected(value) ? "bg-black text-white" : "bg-white text-black"}`, onClick: () => onSelect(value), children: label }, label))) }));
 };
 export default FilterTabs;
--- src/components/CulturalSpace/CulturalSpaceCard.tsx
+++ src/components/CulturalSpace/CulturalSpaceCard.tsx
@@ -8,16 +8,18 @@ interface Props {
 const CulturalSpaceCard = ({ space }: Props) => {
   return (
     <Link to={`/spaces/${space.NUM}`}>
-      <div className="bg-white rounded-xl shadow-md p-3 hover:shadow-lg transition">
+      <div className="bg-white rounded-xl shadow-md p-3 hover:shadow-lg transition h-full">
         <img
           src={space.MAIN_IMG}
           alt={space.FAC_NAME}
           className="w-full h-48 object-cover rounded-md"
         />
-        <div className="mt-2 leading-snug line-clamp-2">
-          <h3 className="font-bold text-lg">{space.FAC_NAME}</h3>
-          <p className="text-sm text-gray-600">{space.ADDR}</p>
-          <p className="text-xs mt-1 text-gray-400">{space.SUBJCODE}</p>
+        <div className="mt-2 leading-snug ">
+          <h3 className="font-bold text-lg truncate">{space.FAC_NAME}</h3>
+          <p className="text-sm text-gray-600 truncate">{space.ADDR}</p>
+          <p className="text-xs mt-1 text-gray-400 truncate">
+            {space.SUBJCODE}
+          </p>
         </div>
       </div>
     </Link>
