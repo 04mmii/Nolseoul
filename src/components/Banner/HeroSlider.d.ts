08f5a00ede8f2cf3d33fd40b82f600c0fcdc3b59
04mmii <04mmii@naver.com>
2025-06-02 14:27:29 +0900

    메인배너 수정

--- src/components/Banner/HeroSlider.d.ts
+++ src/components/Banner/HeroSlider.d.ts
@@ -1,5 +1,6 @@
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "@/styles/swiper.css";
 declare const HeroSlider: () => import("react/jsx-runtime").JSX.Element;
 export default HeroSlider;
